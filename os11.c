a2fb8f3197808f717f05f4424fc23bc7f909d9ca
samiulla1111 <44118231+samiulla1111@users.noreply.github.com>
2019-04-15 10:33:24 +0530

    Add files via upload

---
+++ os11.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+//Resource Request algorithm
+void res_request(int A[10][10],int N[10][10],int AV[10][10],int pid,int m)
+{
+	int reqmat[1][10];
+	int i;
+	printf("\n Enter additional request :- \n");
+	for(i=0;i<m;i++){
+		printf(" Request for resource %d : ",i+1);
+		scanf("%d",&reqmat[0][i]);
+	}
+	
+	for(i=0;i<m;i++)
+		if(reqmat[0][i] > N[pid][i]){
+			printf("\n Error encountered.\n");
+			exit(0);
+	}
+
+	for(i=0;i<m;i++)
+		if(reqmat[0][i] > AV[0][i]){
+			printf("\n Resources unavailable.\n");
+			exit(0);
+		}
+	
+	for(i=0;i<m;i++){
+		AV[0][i]-=reqmat[0][i];
+		A[pid][i]+=reqmat[0][i];
+		N[pid][i]-=reqmat[0][i];
+	}
+}
